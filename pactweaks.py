b5b5d899439c50f256d217591273a13242a1b560
HADEON <52324046+h8d13@users.noreply.github.com>
2025-06-26 13:27:39 +0200

    #  just style

--- pactweaks.py
+++ pactweaks.py
@@ -3,7 +3,6 @@
 lines = open("/etc/pacman.conf").readlines()
 new = []
 has_candy = any("ILoveCandy" in l for l in lines)
-in_multi = False
 
 for l in lines:
     s = l.strip()
@@ -15,19 +14,6 @@ for l in lines:
     if s == "# Misc options" and not has_candy:
         new += [l, "ILoveCandy\n"]
         continue
-
-    if s == "#[multilib]":
-        new.append("[multilib]\n")
-        in_multi = True
-        continue
-
-    if in_multi and s.startswith("#Include"):
-        new.append("Include = /etc/pacman.d/mirrorlist\n")
-        in_multi = False
-        continue
-
-    if s == "[multilib]":
-        in_multi = False  # already enabled
 
     new.append(l)
 
